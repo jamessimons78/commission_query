bece933ac62fada5e702f8b05f6ed9d6104ffe26
jamessimons78 <36401233+jamessimons78@users.noreply.github.com>
2018-12-12 23:35:06 +0800

    修改了配置文件数据库路径

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-class DevConfig(object):
-    DATABASE = 'data/rtkj.db'
+class Config(object):
+	pass
+
+class DevConfig(Config):
+    DATABASE = r'D:\my_web_project\data\rtkj.db'
     SECRET_KEY = '\xcf\xeeL6\n\x97\x0b\x04\xd2\xae\xe4T\xa3\x0b;z\x08\x14\xb2Qa\x83\x98\x04'
     DEBUG = True
     HOST = '0.0.0.0'
